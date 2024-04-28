564601176a7cf78b5970173a5e574da65615358e
sujnkim <blackout3210@naver.com>
2024-04-28 21:26:42 +0900

    fix: RiskCalculator 로직 수정
    자동차 데이터만 이용해 RiskCalculator를 생성하도록 수정
    기존에는 스몸비 리스트에 대해 위험도 리스트를 반환했음
    스몸비 데이터 하나에 대해 위험도를 계산하고 리턴하도록 수정

--- src/main/java/com/capston/v2psmombie/riskCalculate/RiskCalculator.java
+++ src/main/java/com/capston/v2psmombie/riskCalculate/RiskCalculator.java
@@ -1,49 +1,25 @@
 package com.capston.v2psmombie.riskCalculate;
 
 import com.capston.v2psmombie.domain.User;
-import lombok.Builder;
-import lombok.Getter;
 
-import java.util.List;
-import java.util.ArrayList;
-
-@Builder
-@Getter
 public class RiskCalculator {
-    private List<User> smombieDataList;
     private User carData;
 
-    @Builder
-    public RiskCalculator(
-            List<User> smombieDataList,
-            User carData
-    ) {
-        this.smombieDataList = smombieDataList;
+    public RiskCalculator(User carData){
         this.carData = carData;
     }
 
-    public static RiskCalculatorBuilder builder(List<User> users, User car) {
-        RiskCalculatorBuilder calculatorBuilder = new RiskCalculatorBuilder();
-        calculatorBuilder.smombieDataList(users);
-        calculatorBuilder.carData(car);
-        return calculatorBuilder;
-    }
 
-
-    public List<Integer> riskCheck() {
-        List<Integer> riskList = new ArrayList<>();
-        for (User user : smombieDataList) {
-            double minMeetingTime = MeetingCalculator.timeToMeet(user, carData);
-            if (minMeetingTime < 0 || minMeetingTime > 40) {
-                riskList.add(4);
-            } else if (minMeetingTime > 30) {
-                riskList.add(3);
-            } else if (minMeetingTime > 15) {
-                riskList.add(2);
-            } else {
-                riskList.add(1);
-            }
+    public Integer riskCalculate(User user) {
+        double minMeetingTime = MeetingCalculator.timeToMeet(user, carData);
+        if (minMeetingTime < 0 || minMeetingTime > 40) {
+            return 4;
+        } else if (minMeetingTime > 30) {
+            return 3;
+        } else if (minMeetingTime > 15) {
+            return 2;
+        } else {
+            return 1;
         }
-        return riskList;
     }
 }
